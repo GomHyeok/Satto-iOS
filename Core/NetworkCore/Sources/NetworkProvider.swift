2e87e50952479607ed920f512779ede319ccd3d5
TTOzzi <devTTOzzi@gmail.com>
2025-07-26 10:24:22 +0900

    [NL-14]: 네트워크 통신 로직 구현 (#16)
    * [NL-14]: Moya Async 인터페이스 추가
    
    * [NL-14]: 네트워크 통신 로직 구현

---
+++ Core/NetworkCore/Sources/NetworkProvider.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkProvider.swift
+//  NetworkCore
+//
+//  Created by ttozzi on 7/20/25.
+//
+
+import Foundation
+import Moya
+
+public final class NetworkProvider {
+  
+  public static let shared: NetworkProvider = .init(internalProvider: MoyaProvider<MultiTarget>())
+  private let internalProvider: MoyaProvider<MultiTarget>
+  
+  public init(internalProvider: MoyaProvider<MultiTarget>) {
+    self.internalProvider = internalProvider
+  }
+  
+  public func request<T: BaseTargetType>(target: T) async throws -> T.Response {
+    // TODO: Reachability 확인 필요할지
+    do {
+      let responseData = try await internalProvider.request(MultiTarget(target))
+      let response = try JSONDecoder().decode(T.Response.self, from: responseData.data)
+      return response
+    } catch {
+      throw mapToNetworkError(error)
+    }
+  }
+  
+  private func mapToNetworkError(_ error: Error) -> NetworkError {
+    if let moyaError = error as? MoyaError {
+      switch moyaError {
+      case .underlying(let nsError as NSError, _):
+        if nsError.domain == NSURLErrorDomain {
+          switch nsError.code {
+          case NSURLErrorNotConnectedToInternet:
+            return .noInternet
+          case NSURLErrorTimedOut:
+            return .timeout
+          default:
+            return .unknown(nsError)
+          }
+        } else {
+          return .unknown(nsError)
+        }
+      case .statusCode(let response):
+        return .serverError(statusCode: response.statusCode)
+      case .requestMapping, .parameterEncoding, .objectMapping, .encodableMapping:
+        return .invalidResponse
+      default:
+        return .unknown(moyaError)
+      }
+    } else if error is DecodingError {
+      return .decodingError
+    } else {
+      return .unknown(error)
+    }
+  }
+}
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,7 +18,7 @@ import PackageDescription
 let package = Package(
   name: "Satto",
   dependencies: [
-    .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0"),
+    .package(url: "https://github.com/Moya/Moya.git", from: "15.0.3"),
     .package(url: "https://github.com/devxoul/Then.git", from: "3.0.0"),
     .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
     .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1"),
