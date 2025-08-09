86ef47f5af71b8fe3df78beadf3d2bd700613b2e
TTOzzi <devTTOzzi@gmail.com>
2025-08-09 12:57:17 +0900

    [NONE]: Typography computed property 로 수정 (#27)
    (cherry picked from commit 1632029e67cebaa92525852e12877314ada4ba57)

--- DesignSystem/DesignSystem/Sources/Typography.swift
+++ DesignSystem/DesignSystem/Sources/Typography.swift
@@ -11,115 +11,167 @@ import UIKit
 public struct Typography {
 
   // MARK: - Display
-  public static let Display_28_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 28)
-    $0.kerning = .point(0.42)
+  public static var Display_28_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 28)
+      $0.kerning = .point(0.42)
+    }
   }
-  public static let Display_26_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 26)
-    $0.kerning = .point(0.39)
+  public static var Display_26_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 26)
+      $0.kerning = .point(0.39)
+    }
   }
 
   // MARK: - Heading
-  public static let Heading_24_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 24)
-    $0.kerning = .point(0.36)
+  public static var Heading_24_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 24)
+      $0.kerning = .point(0.36)
+    }
   }
-  public static let Heading_24_SB = Style {
-    $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 24)
-    $0.kerning = .point(0.36)
+  public static var Heading_24_SB: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 24)
+      $0.kerning = .point(0.36)
+    }
   }
-  public static let Heading_22_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 22)
-    $0.kerning = .point(0.33)
+  public static var Heading_22_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 22)
+      $0.kerning = .point(0.33)
+    }
   }
-  public static let Heading_22_SB = Style {
-    $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 22)
-    $0.kerning = .point(0.33)
+  public static var Heading_22_SB: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 22)
+      $0.kerning = .point(0.33)
+    }
   }
-  public static let Heading_20_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 20)
-    $0.kerning = .point(0.3)
+  public static var Heading_20_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 20)
+      $0.kerning = .point(0.3)
+    }
   }
-  public static let Heading_20_SB = Style {
-    $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 20)
-    $0.kerning = .point(0.3)
+  public static var Heading_20_SB: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 20)
+      $0.kerning = .point(0.3)
+    }
   }
-  public static let Heading_20_M = Style {
-    $0.font = DesignSystemFontFamily.Suit.medium.font(size: 20)
-    $0.kerning = .point(0.3)
+  public static var Heading_20_M: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.medium.font(size: 20)
+      $0.kerning = .point(0.3)
+    }
   }
-  public static let Heading_20_R = Style {
-    $0.font = DesignSystemFontFamily.Suit.regular.font(size: 20)
-    $0.kerning = .point(0.3)
+  public static var Heading_20_R: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.regular.font(size: 20)
+      $0.kerning = .point(0.3)
+    }
   }
 
   // MARK: - Body
-  public static let Body_18_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 18)
-    $0.kerning = .point(0.27)
+  public static var Body_18_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 18)
+      $0.kerning = .point(0.27)
+    }
   }
-  public static let Body_18_SB = Style {
-    $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 18)
-    $0.kerning = .point(0.27)
+  public static var Body_18_SB: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 18)
+      $0.kerning = .point(0.27)
+    }
   }
-  public static let Body_18_M = Style {
-    $0.font = DesignSystemFontFamily.Suit.medium.font(size: 18)
-    $0.kerning = .point(0.27)
+  public static var Body_18_M: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.medium.font(size: 18)
+      $0.kerning = .point(0.27)
+    }
   }
-  public static let Body_18_R = Style {
-    $0.font = DesignSystemFontFamily.Suit.regular.font(size: 18)
-    $0.kerning = .point(0.27)
+  public static var Body_18_R: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.regular.font(size: 18)
+      $0.kerning = .point(0.27)
+    }
   }
-  public static let Body_16_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 16)
-    $0.kerning = .point(0.24)
+  public static var Body_16_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 16)
+      $0.kerning = .point(0.24)
+    }
   }
-  public static let Body_16_SB = Style {
-    $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 16)
-    $0.kerning = .point(0.24)
+  public static var Body_16_SB: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 16)
+      $0.kerning = .point(0.24)
+    }
   }
-  public static let Body_16_M = Style {
-    $0.font = DesignSystemFontFamily.Suit.medium.font(size: 16)
-    $0.kerning = .point(0.24)
+  public static var Body_16_M: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.medium.font(size: 16)
+      $0.kerning = .point(0.24)
+    }
   }
-  public static let Body_16_R = Style {
-    $0.font = DesignSystemFontFamily.Suit.regular.font(size: 16)
-    $0.kerning = .point(0.24)
+  public static var Body_16_R: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.regular.font(size: 16)
+      $0.kerning = .point(0.24)
+    }
   }
-  public static let Body_14_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 14)
-    $0.kerning = .point(0.21)
+  public static var Body_14_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 14)
+      $0.kerning = .point(0.21)
+    }
   }
-  public static let Body_14_SB = Style {
-    $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 14)
-    $0.kerning = .point(0.21)
+  public static var Body_14_SB: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 14)
+      $0.kerning = .point(0.21)
+    }
   }
-  public static let Body_14_M = Style {
-    $0.font = DesignSystemFontFamily.Suit.medium.font(size: 14)
-    $0.kerning = .point(0.21)
+  public static var Body_14_M: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.medium.font(size: 14)
+      $0.kerning = .point(0.21)
+    }
   }
-  public static let Body_14_R = Style {
-    $0.font = DesignSystemFontFamily.Suit.regular.font(size: 14)
-    $0.kerning = .point(0.21)
+  public static var Body_14_R: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.regular.font(size: 14)
+      $0.kerning = .point(0.21)
+    }
   }
 
   // MARK: - Caption
-  public static let Caption_12_B = Style {
-    $0.font = DesignSystemFontFamily.Suit.bold.font(size: 12)
-    $0.kerning = .point(0.18)
+  public static var Caption_12_B: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.bold.font(size: 12)
+      $0.kerning = .point(0.18)
+    }
   }
-  public static let Caption_12_SB = Style {
-    $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 12)
-    $0.kerning = .point(0.18)
+  public static var Caption_12_SB: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.semiBold.font(size: 12)
+      $0.kerning = .point(0.18)
+    }
   }
-  public static let Caption_12_M = Style {
-    $0.font = DesignSystemFontFamily.Suit.medium.font(size: 12)
-    $0.kerning = .point(0.18)
+  public static var Caption_12_M: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.medium.font(size: 12)
+      $0.kerning = .point(0.18)
+    }
   }
-  public static let Caption_12_R = Style {
-    $0.font = DesignSystemFontFamily.Suit.regular.font(size: 12)
-    $0.kerning = .point(0.18)
+  public static var Caption_12_R: Style {
+    Style {
+      $0.font = DesignSystemFontFamily.Suit.regular.font(size: 12)
+      $0.kerning = .point(0.18)
+    }
   }
 }
 
