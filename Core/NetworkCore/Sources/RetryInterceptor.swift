8d4138d4fd37c160aab67701a8a56726653f156d
TTOzzi <50410213+TTOzzi@users.noreply.github.com>
2025-08-19 16:52:54 +0000

    [Auto] Run swift-format

--- Core/NetworkCore/Sources/RetryInterceptor.swift
+++ Core/NetworkCore/Sources/RetryInterceptor.swift
@@ -10,10 +10,13 @@ import Foundation
 import Moya
 
 final class RetryInterceptor: RequestInterceptor {
-  
+
   private let retryLimit = DefaultConfig.retryLimit
 
-  func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+  func retry(
+    _ request: Request, for session: Session, dueTo error: Error,
+    completion: @escaping (RetryResult) -> Void
+  ) {
     let retryCount = request.retryCount
     if retryCount < retryLimit {
       completion(.retryWithDelay(1 * pow(2, Double(retryCount))))
